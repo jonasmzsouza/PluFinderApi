9e2744e011de62e50f2b97111b774741130c865e
GarciaGP <gabriel.garp@outlook.com>
2021-09-11 00:02:24 -0300

    fix: war to jar

--- src/main/java/br/com/fiap/ambers/PlufinderApi/PlufinderApiApplication.java
+++ src/main/java/br/com/fiap/ambers/PlufinderApi/PlufinderApiApplication.java
@@ -2,9 +2,16 @@ package br.com.fiap.ambers.PlufinderApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlufinderApiApplication {
+public class PlufinderApiApplication extends SpringBootServletInitializer {
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+        return springApplicationBuilder.sources(PlufinderApiApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlufinderApiApplication.class, args);
