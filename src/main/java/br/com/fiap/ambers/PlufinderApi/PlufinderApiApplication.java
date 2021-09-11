958eb8a3868609e841b42dc27c5fe76e3e20a188
GarciaGP <gabriel.garp@outlook.com>
2021-09-11 15:17:04 -0300

    feat: swagger

--- src/main/java/br/com/fiap/ambers/PlufinderApi/PlufinderApiApplication.java
+++ src/main/java/br/com/fiap/ambers/PlufinderApi/PlufinderApiApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableCaching
+@EnableSwagger2
 public class PlufinderApiApplication extends SpringBootServletInitializer {
 	
     @Override
